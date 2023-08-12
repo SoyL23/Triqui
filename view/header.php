49db03a47c146d714a0dbc0859a3d6f999588bea
JonhDogg16 <jsperez071@misena.com>
2023-08-12 17:39:31 -0500

    header

--- view/header.php
+++ view/header.php
@@ -1,3 +1,14 @@
 <?php
 /** */
 ?>
+
+
+<header class="d-flex justify-content-between">
+    <div>
+        <h2>triqui</h2>
+    </div>
+    <nav>
+        <a href=""></a>
+        <a href="">Reiniciar</a>
+    </nav>
+</header>
