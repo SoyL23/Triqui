c2c8491088a8bff8ba7f9bb5b6104a3f82c4e028
Luis A. Guzmán <111367000+SoyL23@users.noreply.github.com>
2023-08-12 21:41:13 -0500

    captura de click

---
+++ controller/capturaClickEnTabla.js
@@ -0,0 +1,8 @@
+const tableDate = document.querySelectorAll("td");
+tableDate.forEach(td => {
+td.addEventListener('click', (evento)=>{
+    const Idtable = evento.target.id;
+    console.log(evento);
+    console.log(Idtable);
+})
+})
--- view/main.php
+++ view/main.php
@@ -2,22 +2,42 @@
 /** */
 ?>
 
-<main class="container">
-    <section class="row">
-        <div class="col-lg-12">
-            <div class="w-100">
-                <div class="card">
-                    <table>
+<main class="p-3">
+    <section class="d-flex justify-content-center p-5">
 
-                    </table>
+        <div class="d-flex justify-content-center align-items-center flex-column w-75 p-3 card h-100 border border-primary mt-3">
+        
+            <table class="w-25">
+
+                <tr>
+                    <td id="table_date_1" class="border border-primary p-4"></td>
+                    <td id="table_date_2" class="border border-primary p-4"></td>
+                    <td id="table_date_3" class="border border-primary p-4"></td>
+                </tr>
+                <tr>
+                    <td id="table_date_4" class="border border-primary p-4"></td>
+                    <td id="table_date_5" class="border border-primary p-4"></td>
+                    <td id="table_date_6" class="border border-primary p-4"></td>
+                </tr>
+                <tr>
+                    <td id="table_date_7" class="border border-primary p-4"></td>
+                    <td id="table_date_8" class="border border-primary p-4"></td>
+                    <td id="table_date_9" class="border border-primary p-4"></td>
+                </tr>
+
+            </table>
+                
+            <script src="controller/capturaClickEnTabla.js"></script>
+
+            <div id="players" class="d-flex justify-content-evenly my-4 w-100">
+                <div class="w-25 border border-primary p-3 text-center bg-success bg-gradient text-white">
+                    <h2>Player 1</h2>
+                </div>
+                <div class="w-25 border border-primary p-3 text-center bg-success bg-gradient text-white">
+                    <h2>Player 2</h2>
                 </div>
             </div>
-            <div class="w-50">
 
-            </div>
-            <div class="w-50">
-
-            </div>
         </div>
     </section>
 </main>
